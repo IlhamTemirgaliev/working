5e3d12486a6a3a447850503534e144225db0a439
Islam Temirgaliev <islam2004tr@mail.ru>
2023-05-13 14:43:29 +0600

    commit cards

--- src/components/Glavnaa/Glavnaa.jsx
+++ src/components/Glavnaa/Glavnaa.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './glavnaa.module.css'
+import { Cards } from '../Cards/Cards'
 const Glavnaa = () => {
   return (
     <div className={styles.conteiner}>
-        <div className={styles.blok_card}></div>
+        <h1 className={styles.title}>Ваши задачи</h1>
+        <div className={styles.blok_card}>
+            <Cards/>
+            <Cards/>
+            <Cards/>
+            <Cards/>
+            <Cards/>
+        </div>
     </div>
   )
 }
